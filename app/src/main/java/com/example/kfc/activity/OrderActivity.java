fcc7e68386de250c55a5e749f47864bc4aef9d25
Sabbir Ahmad <sabbir.sio@gmail.com>
2021-05-02 13:07:17 +0530

    implemented custom action bar using toolbar, added menu

---
+++ app/src/main/java/com/example/kfc/activity/OrderActivity.java
@@ -0,0 +1,25 @@
+package com.example.kfc.activity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.kfc.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //set back button to action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
