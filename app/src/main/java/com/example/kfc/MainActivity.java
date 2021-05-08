ff1c0a538ab7119d2df99f8818f5e1c2a0e217f9
Sabbir Ahmad <sabbir.sio@gmail.com>
2021-05-08 19:37:47 +0530

    added tab layout

--- app/src/main/java/com/example/kfc/MainActivity.java
+++ app/src/main/java/com/example/kfc/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.kfc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
@@ -11,7 +14,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.kfc.activity.OrderActivity;
-import com.example.kfc.adapter.SectionsPagerAdapter;
+import com.example.kfc.fragment.PastaFragment;
+import com.example.kfc.fragment.PizzaFragment;
+import com.example.kfc.fragment.StoreFragment;
+import com.example.kfc.fragment.TopFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(sectionsPagerAdapter);
+
+        //attach tablayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -52,4 +63,48 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position){
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoreFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.pizza_tab);
+                case 2:
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+                    return getResources().getText(R.string.store_tab);
+            }
+            return null;
+        }
+    }
+
 }
--- app/src/main/java/com/example/kfc/adapter/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/kfc/adapter/SectionsPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.kfc.adapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -10,30 +11,39 @@ import com.example.kfc.fragment.PizzaFragment;
 import com.example.kfc.fragment.StoreFragment;
 import com.example.kfc.fragment.TopFragment;
 
-public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-    public SectionsPagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-
-        switch (position){
-            case 0:
-                return new TopFragment();
-            case 1:
-                return new PizzaFragment();
-            case 2:
-                return new PastaFragment();
-            case 3:
-                return new StoreFragment();
-        }
-        return null;
-    }
-
-    @Override
-    public int getCount() {
-        return 4;
-    }
-}
+//public class SectionsPagerAdapter extends FragmentPagerAdapter {
+//
+//    public SectionsPagerAdapter(FragmentManager fm) {
+//        super(fm);
+//    }
+//
+//    @Override
+//    public Fragment getItem(int position) {
+//
+//        switch (position){
+//            case 0:
+//                return new TopFragment();
+//            case 1:
+//                return new PizzaFragment();
+//            case 2:
+//                return new PastaFragment();
+//            case 3:
+//                return new StoreFragment();
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return 4;
+//    }
+//
+//    @Override
+//    public CharSequence getPageTitle(int position) {
+//        switch (position){
+//            case 0:
+//                return get
+//        }
+//        return super.getPageTitle(position);
+//    }
+//}
